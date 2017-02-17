5502e092d726f43cc1294966edcd5a783ce07f6a
ghigbie <gehigbie@gmail.com>
2017-02-17 01:01:18 +0000

    set up server to listen and render pages

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+   res.render("home"); 
+});
+
+app.get("*", function(req, res){
+   res.render("notFound"); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The server is up and running! Yay!!!");
+});
