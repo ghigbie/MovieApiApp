65b9382970a7595bf435406ccf1c354ce25fa81e
ghigbie <gehigbie@gmail.com>
2017-02-17 22:54:07 +0000

    made search request dynamically load page

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ app.get("/", function(req, res){
 });
 
 app.get("/results", function(req, res){
-    request("http://www.omdbapi.com/?s=ocean", function(error, response, body){
+    console.log(req.query.searchTitle);
+    var query = req.query.searchTitle;
+    var url = "http://www.omdbapi.com/?s=" + query;
+    request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
             var data = JSON.parse(body);
             res.render("results", {data: data});
