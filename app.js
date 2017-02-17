8c896a92c26337a74746d3aa507dbb2446c64c5a
ghigbie <gehigbie@gmail.com>
2017-02-17 01:49:55 +0000

    working on results page

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/results", function(req, res){
     request("http://www.omdbapi.com/?s=star", function(error, response, body){
         if(!error && response.statusCode == 200){
             var results = JSON.parse(body);
-            res.send(results);
+            res.render("results");
         }
     });
     
